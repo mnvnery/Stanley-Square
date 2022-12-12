d015eb2c58cea554ff6ed62f42285a79a61d34fe
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2022-12-12 22:25:42 +0000

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,31 @@ import Header from '../components/Header'
 import '../styles/globals.css'
 import '../styles/faq.css'
 import Head from 'next/head'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
     <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+        />
+
+        <Script id="ga-script" strategy="lazyOnload">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+                `}
+        </Script>
     </Head>
+
+
     <Header/>
     <Component {...pageProps} />
     </>
